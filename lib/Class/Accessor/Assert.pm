811f86886b94edd8f2f617a3bbc717b1a4724c76
Steffen Müller <smueller@cpan.org>
2005-01-29 12:15:36 +0000

    Import of SMUELLER/Class-Accessor-Assert-1.30 from CPAN.
    gitpan-cpan-distribution: Class-Accessor-Assert
    gitpan-cpan-version:      1.30
    gitpan-cpan-path:         SMUELLER/Class-Accessor-Assert-1.30.tar.gz
    gitpan-cpan-author:       SMUELLER
    gitpan-cpan-maturity:     released

--- lib/Class/Accessor/Assert.pm
+++ lib/Class/Accessor/Assert.pm
@@ -4,58 +4,123 @@ use strict;
 use warnings;
 use base qw(Class::Accessor Class::Data::Inheritable);
 use Carp qw(croak confess);
-our $VERSION = '1.21';
+our $VERSION = '1.30';
 
 sub _mk_accessors {
-    my ($self, $maker, @fields) = @_;
-    $self->mk_classdata("accessor_specs") 
-        unless $self->can("accessor_specs");
-        
+    my ( $self, $maker, @fields ) = @_;
+    $self->mk_classdata("accessor_specs")
+      unless $self->can("accessor_specs");
+
     my %spec = $self->parse_fields(@fields);
-    $self->accessor_specs({ %spec, %{ $self->accessor_specs || {} }});
-    $self->SUPER::_mk_accessors($maker, keys %spec);
+    $self->accessor_specs( { %spec, %{ $self->accessor_specs || {} } } );
+    $self->SUPER::_mk_accessors( $maker, keys %spec );
+
+    {
+        no strict 'refs';
+
+        # additional methods for magic array methods
+        my $class = ref $self || $self;
+	# Note how we curry the subs with the lexical "$field":
+	# The subs are closures and therefore have access to their lexical
+	# scope. Clarity suffers from this, but the performance should be
+	# about 25% higher than a cleaner approach due to a saved subroutine
+	# call for every ary_*(...) call.
+        for my $field ( grep { $spec{$_}{array} } keys %spec ) {
+	    # foo_push sub
+            *{"${class}::${field}_push"} = sub {
+                my ( $self, @values ) = @_;
+                $self->{$field} = [] unless defined $self->{$field};
+                push @{ $self->{$field} }, @values;
+            };
+	    # foo_pop sub
+            *{"${class}::${field}_pop"} = sub {
+                my ( $self ) = @_;
+                return pop @{ $self->{$field} || [] };
+            };
+	    # foo_unshift sub
+            *{"${class}::${field}_unshift"} = sub {
+                my ( $self, @values ) = @_;
+                $self->{$field} = [] unless defined $self->{$field};
+                unshift @{ $self->{$field} }, @values;
+            };
+	    # foo_shift sub
+            *{"${class}::${field}_shift"} = sub {
+                my ( $self ) = @_;
+                return shift @{ $self->{$field} || [] };
+            };
+        }
+    }
 }
 
 sub new {
-    my ($self, $stuff) = @_;
-    my $not_a_void_context = eval { %{$stuff || {}} };
+    my ( $self, $stuff ) = @_;
+    my $not_a_void_context = eval { %{ $stuff || {} } };
     croak "$stuff doesn't look much like a hash to me" if $@;
-    if ($self->can("accessor_specs")) {
+    if ( $self->can("accessor_specs") ) {
         my $spec = $self->accessor_specs;
-        for my $k (keys %$spec) {
+        for my $k ( keys %$spec ) {
             confess "Required member $k not given to constructor"
-                if $spec->{$k}->{required} and not exists $stuff->{$k};
-            confess "Member $k needs to be of type ".$spec->{$k}->{class}
-                if exists $spec->{$k}->{class} and exists $stuff->{$k}
-                and !UNIVERSAL::isa($stuff->{$k}, $spec->{$k}->{class});
+              if $spec->{$k}->{required}
+              and not exists $stuff->{$k};
+            confess "Member $k needs to be of type " . $spec->{$k}->{class}
+              if exists $spec->{$k}->{class}
+              and exists $stuff->{$k}
+              and !UNIVERSAL::isa( $stuff->{$k}, $spec->{$k}->{class} );
         }
     }
-    return $self->SUPER::new($stuff) 
+    return $self->SUPER::new($stuff);
 }
 
 sub set {
     return shift->SUPER::set(@_) unless $_[0]->can("accessor_specs");
-    my($self, $key) = splice(@_, 0, 2);
+    my ( $self, $key ) = splice( @_, 0, 2 );
     my $spec = $self->accessor_specs;
-    return $self->SUPER::set($key, @_) if !exists $spec->{$key}
-                                       or @_ > 1; # No support for arrays
-    confess "Member $key needs to be of type ".$spec->{$key}->{class}
-        if defined $_[0] and exists $spec->{$key}->{class} and
-            !UNIVERSAL::isa($_[0], $spec->{$key}->{class});
+    return $self->SUPER::set( $key, @_ )
+      if !exists $spec->{$key}
+      or @_ > 1;    # No support for arrays
+    confess "Member $key needs to be of type " . $spec->{$key}->{class}
+      if defined $_[0]
+      and exists $spec->{$key}->{class}
+      and !UNIVERSAL::isa( $_[0], $spec->{$key}->{class} );
+
+    $_[0] = [ $_[0] ]
+      if defined $_[0]
+      and $spec->{$key}->{array}
+      and ref $_[0] ne 'ARRAY';
+
     $self->{$key} = $_[0];
 }
 
+sub get {
+    return shift->SUPER::get(@_) unless $_[0]->can("accessor_specs");
+    my ( $self, $key ) = splice( @_, 0, 2 );
+    my $spec = $self->accessor_specs;
+    return $self->SUPER::get( $key, @_ )
+      if !exists $spec->{$key}
+      or @_ > 1;    # No support for arrays
+    if ( $spec->{$key}{array} ) {
+        wantarray
+          ? @{ $self->SUPER::get( $key, @_ ) || [] }
+          : $self->SUPER::get( $key, @_ );
+    }
+    else {
+        $self->SUPER::get( $key, @_ );
+    }
+}
+
 sub parse_fields {
-    my ($self, @fields) = @_;
+    my ( $self, @fields ) = @_;
     my %spec;
     for my $f (@fields) {
-        my $orig_f = $f; # For error reporting
+        my $orig_f = $f;    # For error reporting
         my %subspec;
+
         # All the tests go here
         $subspec{required} = $f =~ s/^\+//;
         $f =~ s/=(.*)// and $subspec{class} = $1;
-        $f =~ /^\w+$/   
-            or croak "Couldn't understand field specification $orig_f";
+        $subspec{array} = $f =~ s/^\@//;
+        $f =~ /^\w+$/
+          or croak "Couldn't understand field specification $orig_f";
         $spec{$f} = \%subspec;
     }
     return %spec;
@@ -71,7 +136,7 @@ Class::Accessor::Assert - Accessors which type-check
 =head1 SYNOPSIS
 
   use Class::Accessor::Assert;
-  __PACKAGE__->mk_accessors( qw( +foo bar=Some::Class baz ) );
+  __PACKAGE__->mk_accessors( qw( +foo bar=Some::Class baz @bits ) );
 
 =head1 DESCRIPTION
 
@@ -83,6 +148,26 @@ To specify that a member is mandatory in the constructor, prefix its
 name with a C<+>. To specify that it needs to be of a certain class
 when setting that member, suffix C<=CLASSNAME>. Unblessed reference
 types such as C<=HASH> or C<=ARRAY> are acceptable.
+
+To specify that a member is an array, prefix its name with a C<@>.
+These members also have the following four special methods that wrap
+the builtin array operations C<push>, C<pop>, C<unshift>, and 
+C<shift>:
+
+    # for a @bits member:
+    
+    $y->bits_push(@new_values);
+    print $y->bits_pop;
+    
+    $y->bits_unshift(@new_values);
+    print $y->bits_shift;
+
+The C<@> can be combined with the C<+> prefix to make a member that
+is an array that you must set in the constructor. The C<+> must 
+precede the C<@>.
+
+    # 'foo' is required in the constructor
+    __PACKAGE__->mk_accessors(qw( +@foo ));
 
 =head1 SEE ALSO
 
--- t/1.t
+++ t/1.t
@@ -2,38 +2,88 @@
 package Foo;
 use Test::More no_plan;
 use base 'Class::Accessor::Assert';
-__PACKAGE__->_mk_accessors("make_accessor", qw(a=ARRAY +b c=IO::File));
+__PACKAGE__->_mk_accessors( "make_accessor", qw(a=ARRAY +b c=IO::File @d) );
 
 eval { my $x = Foo->new() };
-like ($@, qr/Required member b not given to constructor/, 
-    "Constructor needs to die on required fields");
+like(
+    $@,
+    qr/Required member b not given to constructor/,
+    "Constructor needs to die on required fields"
+);
 
-eval { my $x = Foo->new({ b=> 1}) };
-ok(!$@, "This required field can be anything");
+eval { my $x = Foo->new( { b => 1 } ) };
+ok( !$@, "This required field can be anything" );
 
-eval { my $x = Foo->new({ b=> 1, a => {} }) };
-like($@, qr/Member a needs to be of type ARRAY/, 
-    "But a has to be an array");
+eval { my $x = Foo->new( { b => 1, a => {} } ) };
+like( $@, qr/Member a needs to be of type ARRAY/, "But a has to be an array" );
 
 # Now this is my usual trick
-eval { my $x = Foo->new( a => [], b=> 1234 )};
-like($@, qr/much like/, "Traps the non-hashref case");
+eval { my $x = Foo->new( a => [], b => 1234 ) };
+like( $@, qr/much like/, "Traps the non-hashref case" );
 
 # OK, let's finally get an object.
-my $y = Foo->new({ a=>[], b=> 1234 });
+my $y = Foo->new( { a => [], b => 1234 } );
+
 # Now let's test setting
 
 eval { $y->a("This is evidently not an array ref") };
-like($@, qr/Member a needs to be of type ARRAY/, 
-    "Can't set to prohibited type");
+like(
+    $@,
+    qr/Member a needs to be of type ARRAY/,
+    "Can't set to prohibited type"
+);
 
 eval { $y->c("This is evidently not an IO::File") };
-like($@, qr/Member c needs to be of type IO::File/, 
-    "Can't set to prohibited type");
+like(
+    $@,
+    qr/Member c needs to be of type IO::File/,
+    "Can't set to prohibited type"
+);
 
 use IO::File;
-eval { $y->c(IO::File->new) };
-ok(!$@, "Can set to an allowed type");
+eval { $y->c( IO::File->new ) };
+ok( !$@, "Can set to an allowed type" );
 
+# array fields
+eval { $y->d(qw(cyan yellow magenta black)) };
+ok( !$@, "Array field takes literal array" );
+
+# has the array methods
+can_ok( $y, qw(d_push d_pop d_unshift d_shift) );
+
+is(
+    join( '; ', $y->d ),
+    'cyan; yellow; magenta; black',
+    "Returns the array in array context..."
+);
+like( scalar( $y->d ), qr/ARRAY/, "...and the reference in scalar context" );
+
+$y->d_push(qw(red green blue));
+is(
+    join( '; ', $y->d ),
+    'cyan; yellow; magenta; black; red; green; blue',
+    "Push elements"
+);
+
+is( $y->d_pop, 'blue', "Pop an element" );
+is(
+    join( '; ', $y->d ),
+    'cyan; yellow; magenta; black; red; green',
+    "...and leave the remaining elements"
+);
+
+is( $y->d_shift, 'cyan', "Shift an element" );
+is(
+    join( '; ', $y->d ),
+    'yellow; magenta; black; red; green',
+    "...and leave the remaining elements"
+);
+
+$y->d_unshift('orange');
+is(
+    join( '; ', $y->d ),
+    'orange; yellow; magenta; black; red; green',
+    "Unshift an element"
+);
 
 1
